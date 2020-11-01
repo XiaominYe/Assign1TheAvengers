fdcbc8a207118c93607b2799693083008151e452
XiaominYe <1240014575@qq.com>
2020-11-02 00:07:39 +0800

    Assginment1 second times

---
+++ app/Controllers/Heros.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+class Heros extends \CodeIgniter\Controller {
+
+public function index() {            
+// connect to the model        
+$hero = new \App\Models\Heros();           
+// retrieve all the records  
+$records = $heros->findAll();
+// JSON encode and return the result     
+return json_encode($records);
+} 
+
+ public function showme($id)    
+         {
+           // connect to the model 
+              $hero = new \App\Models\Heros();   
+          // retrieve all the records   
+            $record = $heros->find($id);    
+            
+           // get a template parser  
+               $parser = \Config\Services::parser();   
+              // tell it about the substitions  
+               return $parser->setData($record)      
+                  // and have it render the template with those  
+                 ->render('oneplace'); 
+             } 
+ 
+
+}
+
+
