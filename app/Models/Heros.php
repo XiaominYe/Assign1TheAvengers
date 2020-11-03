e832947fd0b1dffd88a3d53e3bfa22661febf724
XiaominYe <1240014575@qq.com>
2020-11-03 18:13:01 +0800

    Assginment1

--- app/Models/Heros.php
+++ app/Models/Heros.php
@@ -13,42 +13,45 @@ class Heros {
         '1' => [
             'id' => 1,
             'name' => 'IronMan',
-            'description' => 'A special administrative region on the eastern side of the Pearl River estuary in southern China.',
+            'description' => 'Using the Ark reactor as the source of energy, secretly created a suit of armor (Mark1) to escape after breaking out of the siege, and then participating in the creation of the Avengers',
             'link' => 'https://baike.baidu.com/item/%E9%92%A2%E9%93%81%E4%BE%A0/303?fr=aladdin',
             'image' => 'IronMan.png',
+            'team' => 'The Avengers',
+            
         ],
         '2' => [
             'id' => 2,
             'name' => 'CaptainAmerica',
-            'description' => 'A special administrative region on the western side of the Pearl River estuary in southern China.',
+            'description' => 'Later, in an action at the end of World War II, Captain America fought fiercely with his rival Red Skull, and fell into the sea and was frozen for nearly 70 years. He did not join the Avengers until he was discovered by SHIELD and thawed.',
             'link' => 'https://baike.baidu.com/item/%E7%BE%8E%E5%9B%BD%E9%98%9F%E9%95%BF/3472?fr=aladdin',
             'image' => 'CaptainAmerica.png',
         ],
         '3' => [
             'id' => 3,
             'name' => 'GreenGiant',
-            'description' => 'A country at the centre of the Southeast Asian Indochinese peninsula composed of 76 provinces.',
+            'description' => 'Rick Jones, the young friend of the early Hulk, has an honorary identity;
+Originally joined as a villain undercover, Wonder Man (Wonder Man), later changed his evil spirits;',
             'link' => 'https://baike.baidu.com/item/%E7%BB%BF%E5%B7%A8%E4%BA%BA/65931?fr=aladdin',
             'image' => 'thailand.jpg',
         ],
         '4' => [
             'id' => 4,
             'name' => 'BlackWidow',
-            'description' => 'An island country in East Asia.',
+            'description' => 'The rebellious Russian spy Black Widow.',
             'link' => 'https://baike.baidu.com/item/%E9%BB%91%E5%AF%A1%E5%A6%87/6478268?fr=aladdin',
             'image' => 'BlackWidow.png',
         ],
         '5' => [
             'id' => 5,
             'name' => 'ClintBarton',
-            'description' => 'The easternmost country on the Indochina Peninsula.',
+            'description' => 'He was exceptionally talented. When he was a child, he was awarded the title of "Eagle Eye" and "Best Sniper in the World".',
             'link' => 'https://baike.baidu.com/item/%E9%B9%B0%E7%9C%BC/15107237?fr=aladdin',
             'image' => 'ClintBarton.png',
         ],
          '6' => [
             'id' => 6,
                 'name' => 'Thor',
-            'description' => 'The easternmost country on the Indochina Peninsula.',
+            'description' => 'A gad.',
             'link' => 'https://baike.baidu.com/item/%E9%9B%B7%E7%A5%9E/8321769?fromtitle=%E9%9B%B7%E7%A5%9E%E6%89%98%E5%B0%94&fromid=3804120',
             'image' => 'Quill.png',
              
