6c18408a2cc7f45cd5e13405ccf86194fd4dabae
XiaominYe <1240014575@qq.com>
2020-11-03 15:46:53 +0800

    Assginment1 second times

---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Lab 06</title>
+	</head>
+	<body>
+		<h1>Welcome The Avengers</h1>
+		<p><a href="/TheAvengers">Show the heros</a></p>
+		<p><a href="/Heros">Show the heros data</a></p>
+	</body>
+</html>
